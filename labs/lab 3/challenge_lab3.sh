e0e88620ce79a79771441c2fb89828b49d2fcbdd
Prisharana090 <prisharana090@gmail.com>
2024-10-14 12:08:22 -0400

    lab3 done

--- labs/lab 3/challenge_lab3.sh
+++ labs/lab 3/challenge_lab3.sh
@@ -1,16 +1,17 @@
-# Author: Prisha Rana 
-# Date: 10/10/2024
-# Description: This is a basic script that
-# uses environment variables and the date command
-
 #!/bin/bash
-echo "This is a shell script,"
-echo -n "This script will print the current date"
-date
-echo "This is the machine's name:"
-uname -n
-echo "This is the current user:"
-echo $USER
-echo "This is the current date"
-date +"%A, %B %d %R %Z"
+echo "Basic information about my system"
+echo "Date and time in UTC and Local:"
+echo "10/10/24 12:07:07 UTC"
+
+echo "some of the information about my system:"
+uname -a
+
+echo " "
+echo "RAM Information"
+free -h
+
+echo "Disk Space Usage"
+df -h
+figlet DONE
+
 
