49f27c4bb48cc2f7e7a6c85c3684f78c8e4086a7
Prisharana090 <prisharana090@gmail.com>
2024-11-08 00:43:47 -0500

    lab3 wr3 notes3 done

---
+++ labs/lab3/lab3-script1.sh
@@ -0,0 +1,14 @@
+# Author : Prisha Rana
+# Date: 11/7/2024
+#Description: This is a basic script. 
+#!/bin/bash
+echo "This is a shell script" 
+echo -n "this script will print the current date"
+date
+echo "this is the machine's name"
+uname -n 
+echo "this is current user:"
+echo $USER 
+echo "this is the current date"
+date + "%A, %B %d %R %Z"
+
